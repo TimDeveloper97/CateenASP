d51003106c185697b26fc2dbde8dfa12ba70c419
TimDeveloper97 <timdeveloper97@gmail.com>
2021-12-13 17:51:51 +0700

    create project

---
+++ CanteenASP/CanteenASP/Controllers/SigninController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanteenASP.Controllers
+{
+    public class SigninController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

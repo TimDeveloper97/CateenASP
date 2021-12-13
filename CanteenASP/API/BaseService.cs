3de0585adf8a0ce487cdaea53d96c1daef905fdb
TimDeveloper97 <timdeveloper97@gmail.com>
2021-12-13 19:26:15 +0700

    connect môngDB

---
+++ CanteenASP/API/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    internal class BaseService
+    {
+        public BaseService()
+        {
+            var client = new MongoClient("mongodb + srv://timdeveloper:<password>@cluster0.6ejvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        }
+    }
+}

e18ce7c27bfeb0879a29ed01690fe370e3e09ab1
TimDeveloper97 <timdeveloper97@gmail.com>
2021-12-17 17:37:13 +0700

    time meal setup

--- CanteenASP/API/Common.cs
+++ CanteenASP/API/Common.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -26,6 +27,32 @@ namespace API
                 }
                 return sb.ToString();
             }
+        }
+
+        public static MealTime TimeToEnum(DateTime orderTime)
+        {
+            var tBreakfast = new RangeTime { Start = new TimeSpan(0, 0, 0), Finish = new TimeSpan(7, 0, 0) };
+            var tLunch = new RangeTime { Start = new TimeSpan(0, 0, 0), Finish = new TimeSpan(11, 0, 0) };
+            var tDinner = new RangeTime { Start = new TimeSpan(0, 0, 0), Finish = new TimeSpan(17, 0, 0) };
+
+            var current = orderTime.TimeOfDay;
+            if(current >= tBreakfast.Start 
+                && current <= tBreakfast.Finish)
+            {
+                return MealTime.Breakfast;
+            }
+            else if(current >= tLunch.Start
+                && current <= tLunch.Finish)
+            { 
+                return MealTime.Lunch;
+            }
+            else if(current >= tDinner.Start
+                && current <= tDinner.Finish)
+            {
+                return MealTime.Dinner;
+            }
+
+            return null;
         }
     }
 }
---
+++ CanteenASP/Model/Order.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum MealTime
+    {
+        Breakfast = 0,
+        Lunch,
+        Dinner,
+    }
+
+    public class RangeTime
+    {
+        public TimeSpan Start { get; set; }
+        public TimeSpan Finish { get; set; }
+    }
+
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public User? User { get; set; }
+        public Food? Food { get; set; }
+        public MealTime Type { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}

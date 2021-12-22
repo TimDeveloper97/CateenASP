81ccce19ee7a15c3d2d25c09523e36dbd51f4385
thang1224 <thanguk04@gmail.com>
2021-12-22 01:37:12 -0800

    fix view cpn

---
+++ CanteenASP/CanteenASP/Controllers/Components/MealsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace CanteenASP.Controllers.Components
+{
+    public class MealsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(List<Order> listOrder)
+        {
+            return View("Default", listOrder);
+        }
+    }
+}

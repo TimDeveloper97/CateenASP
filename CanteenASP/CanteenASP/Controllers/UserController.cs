70a5630ad6d544e32bff1a8899b9546a24fce6db
thang1224 <thanguk04@gmail.com>
2021-12-25 03:03:26 -0800

    complete user template

--- CanteenASP/CanteenASP/Controllers/UserController.cs
+++ CanteenASP/CanteenASP/Controllers/UserController.cs
@@ -65,12 +65,76 @@ namespace CanteenASP.Controllers
         [HttpPost]
         public async Task<IActionResult> Forgot(Model.User user)
         {
-            var res = await _userService.ResetPassword(user.UserName, user.Password, user.Phone);
+            var res = await _userService.ForgotPassword(user.UserName, user.Password, user.Phone);
             if (res.Success)
             {
                 return RedirectToAction("Index", "User");
             }
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Reset()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Reset(string oldPassword, string password)
+        {
+            var userId = HttpContext.Session.GetString("UserId");
+            if(string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Index");
+            }
+            var res = await _userService.ResetPassword(userId, oldPassword, password);
+            if (!res.Success)
+            {
+               
+                ViewData["Message"] = res.Message;
+                return View();
+            }
+            ViewData["Message"] = res.Message;
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Infor()
+        {
+            var userId = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Index");
+            }
+            var user = await _userService.GetItem(userId);
+            return View(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ChangeInfor()
+        {
+
+            var userId = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Index");
+            }
+            var user = await _userService.GetItem(userId);
+            return View(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangeInfor(Model.User user)
+        {
+            user.DisplayName = user.LastName + " " + user.FirstName;
+            var userId = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Index");
+            }
+            user.Id = userId;
+            var res = await _userService.ChangeInfor(user);
+            if (res)
+            {
+                return RedirectToAction("Infor");
+            }
+            return View(user);
         }
     }
 }

36ab55c5525da4bbb96ec409a10ed26d2b2e960b
thang1224 <thanguk04@gmail.com>
2022-01-20 11:27:34 +0700

    complete new UI food type combo

---
+++ CanteenASP/API/Helper/OrderPayload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Helper
+{
+    public class OrderPayload
+    {
+        public string Id { get; set; }
+
+        public string Size { get; set; }
+    }
+}

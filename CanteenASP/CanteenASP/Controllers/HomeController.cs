8ef7154778bf477e3f6e2ea6a63a5f766b9abaac
thang1224 <thanguk04@gmail.com>
2022-01-20 11:30:59 +0700

    add base controller to home

--- CanteenASP/CanteenASP/Controllers/HomeController.cs
+++ CanteenASP/CanteenASP/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using System.Globalization;
 
 namespace CanteenASP.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
         private readonly FoodService _foodService;
